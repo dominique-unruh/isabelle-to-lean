a88fab39b254668bed1a0fa1dd3c62c31a5ba01b
Dominique Unruh <unruh@ut.ee>
2022-11-02 02:28:03 +0200

    Added stopwatch.

--- src/main/scala/isabelle2lean/Main.scala
+++ src/main/scala/isabelle2lean/Main.scala
@@ -43,18 +43,22 @@ object Main {
 
 
   def main(args: Array[String]): Unit = {
-    val stopWatch = new StopWatch("Time spent")
-    stopWatch.start()
+    val stopWatch = StopWatch.createStarted()
 
     // We can get all theorems from a thy (incl ancestors) via "Global_Theory.all_thms_of thy false"
     val thmNames = Seq(
-      "HOL.conjI",
+//      "HOL.conjI",
 //      "Binomial.binomial_eq_0",
-//      "Nat.add_0_right",
+      "Nat.add_0_right",
     )
 
 
     Globals.isabelle.await
+
+    stopWatch.stop()
+    println("Isabelle initialized after: " + stopWatch.formatTime())
+    stopWatch.reset()
+    stopWatch.start()
 
     output.synchronized {
       output.println(preamble)
@@ -76,27 +80,13 @@ object Main {
     defineConstant("HOL.False", "Bool.false")
     defineConstant("Nat.Suc", "Nat.succ")
 
-    /*
-
-    def Pure_imp1 (p:Prop) (q:Prop): Prop := p -> q
-    def Pure_prop1 (p:Prop): Prop := p
-    def Pure_eqprop (p:Prop) (q:Prop): Prop := p = q
-    #reduce axiom_Pure_equal_elim Pure_imp1 Pure_eqprop
-
-    */
-
-//    await(Constants.compute("Pure.imp"))
-//    await(Constants.compute("Pure.eq"))
-
     val futures = thmNames map { thmName =>
       for (thm <- Thm(ctxt, thmName).forceFuture;
            pthm = getPThm(thm);
            theorem <- Theorems.compute(pthm))
       yield {
         println(s"### Finished $thmName")
-//        println(s"### A.k.a. $theorem")
         println(s"# theorems:       ${Theorems.count}")
-//        println(s"# named theorems: ${Theorems.namedCount}")
         println(s"# axioms:         ${Axioms.count}")
         println(s"# constants:      ${Constants.count}")
       }}
@@ -107,9 +97,7 @@ object Main {
     println(s"Done. ${Globals.executor.getActiveCount}")
     ITyp.printStats()
     stopWatch.stop()
-    System.out.println(stopWatch.getTime());
-
-    //    Globals.executor.shutdown()
+    System.out.println("Total time: " + stopWatch.formatTime())
 
     output.synchronized {
       output.close()
@@ -121,35 +109,4 @@ object Main {
   def preamble: String =
 //    IOUtils.toString(getClass.getResource("/preamble.lean"), "utf-8")
     Files.readString(Paths.get("src/main/lean/preamble.lean"))
-
-/*  // TODO: check for duplicate Var/Free with different types
-  // TODO: check for duplicate TFree/TVar with different sorts
-  // TODO use futures
-  def argumentsOfProp_OLD(prop: Term): String = {
-    val vars = IsabelleOps.addVars(prop).retrieveNow.reverse
-    val frees = IsabelleOps.addFrees(prop).retrieveNow.reverse
-    //    if frees.nonEmpty && vars.nonEmpty then
-    //      System.err.println(s"Warning: nonempty vars and frees, not sure how to handle this: ${prop.pretty(ctxt)}")
-    val tfrees = IsabelleOps.addTFrees(prop).retrieveNow.reverse
-    val tvars = IsabelleOps.addTVars(prop).retrieveNow.reverse
-    assert(tfrees.isEmpty)
-
-    val targs = tvars map { case ((name, index), sort) =>
-      // TODO: don't ignore sort!
-      val name2 = mapName(name = name, extra = index, category = Namespace.TVar)
-      s"{$name2 : Type} [Nonempty $name2]"
-    }
-
-    val vars2 = vars map { case ((name, index), typ) =>
-      val name2 = mapName(name = name, extra = index, category = Namespace.Var)
-      s"($name2 : ${ITyp(typ).outputTerm})"
-    }
-
-    val frees2 = frees map { case (name, typ) =>
-      val name2 = mapName(name, category = Namespace.Free)
-      s"($name2 : ${ITyp(typ).outputTerm})"
-    }
-
-    (targs ++ frees2 ++ vars2).mkString(" ")
-  }*/
 }
